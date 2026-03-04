3c5713897315003ef558ff20ab53d1c4683897a3
Darius Legus <darius.legus@vilnius.lt>
2026-03-04 13:38:56 +0200

    Add Terraform configuration for vSphere provider

---
+++ iac/terraform/vsphere/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = "~> 2.6"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
+}
+
+provider "vsphere" {
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  allow_unverified_ssl = true
+}
